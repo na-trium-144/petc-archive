ce2fbf331a9b2035e3cee75a4c67024c1f4f1be8
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2022-10-26 12:10:20 +0900

    404エラーメッセージ

--- api/index.js
+++ api/index.js
@@ -3,26 +3,18 @@ const ejs = require("ejs");
 const app = express();
 const cheerio = require('cheerio');
 const fs = require('fs');
-const iconv = require("iconv-lite");
+const Encoding = require('encoding-japanese');
 
 const path = __dirname + "/..";
 app.use(express.static("public"));
 
-function queryToBuffer(q) {
-  let i = 0;
-  let a = [];
-  while (i < q.length) {
-    if (q.charCodeAt(i) === 37) {
-      a.push(parseInt(q.slice(i + 1, i + 3), 16));
-      i += 3;
-    } else {
-      a.push(q.charCodeAt(i));
-      i += 1;
-    }
-  }
-  return Buffer.from(a);
+function pageTemplate() {
+  return fs.readFileSync(path + "/template.ejs", "utf-8");
 }
 
+function eucToStr(b){
+  return Encoding.codeToString(Encoding.convert(b, {from: "EUC-JP", to: "UNICODE"}));
+}
 function pageExists(page) {
   if (!page.startsWith("/")) {
     return true;
@@ -31,7 +23,7 @@ function pageExists(page) {
   if (page.indexOf("#") >= 0) {
     page = page.slice(0, page.indexOf("#"));
   }
-  page = iconv.decode(queryToBuffer(page), "eucjp");
+  page = eucToStr(Encoding.urlDecode(page));
   if (fs.existsSync(path + "/websites_utf8/wiki.hosiken.jp/" + page + "/index.html")) {
     return true;
   }
@@ -47,9 +39,9 @@ app.get("/petc(3gou)?4?/", (request, response) => {
     page = page.slice(0, page.indexOf("#"));
   }
   page = page.replace("?", "");
-  page = iconv.decode(queryToBuffer(page), "eucjp");
+  page = eucToStr(Encoding.urlDecode(page));
   if (fs.existsSync(path + "/websites_utf8/wiki.hosiken.jp/" + page + "/index.html")) {
-    const $ = cheerio.load(iconv.decode(fs.readFileSync(path + "/websites_utf8/wiki.hosiken.jp/" + page + "/index.html"), "eucjp"));
+    const $ = cheerio.load(eucToStr(fs.readFileSync(path + "/websites_utf8/wiki.hosiken.jp/" + page + "/index.html")));
     for (let a of $("a").get()) {
       let href = $(a).attr('href');
       let title = $(a).attr('title');
@@ -57,13 +49,13 @@ app.get("/petc(3gou)?4?/", (request, response) => {
       if (href) {
         let url = new URL(href, `https://${request.hostname}/`);
         if (url.host === "wiki.hosiken.jp" || url.host === request.hostname) {
-          if (url.searchParams.has("plugin") && url.searchParams.get("plugin") === "ref"){
+          if (url.searchParams.has("plugin") && url.searchParams.get("plugin") === "ref") {
             $(a).replaceWith(`<a href="/ref${request.path}/${url.searchParams.get('page')}/${url.searchParams.get('src')}" title="${title}">${content}</a>`);
-          } else if (url.searchParams.has("plugin") && url.searchParams.has("refer") && url.searchParams.get("plugin") === "attach"){
+          } else if (url.searchParams.has("plugin") && url.searchParams.has("refer") && url.searchParams.get("plugin") === "attach") {
             $(a).replaceWith(`<a href="/ref${request.path}/${url.searchParams.get('refer')}/${url.searchParams.get('openfile')}" title="${title}">${content}</a>`);
-          }else if (pageExists(url.pathname + url.search)) {
+          } else if (pageExists(url.pathname + url.search)) {
             $(a).replaceWith(`<a href="${url.pathname + url.search}" title="${title}">${content}</a>`);
-          }else{
+          } else {
             $(a).replaceWith(`<a style="color: red;" href="${url.href}" title="${title}">${content}</a>`)
           }
         }
@@ -78,9 +70,9 @@ app.get("/petc(3gou)?4?/", (request, response) => {
       if (href) {
         let url = new URL(href, `https://${request.hostname}/`);
         if (url.host === "wiki.hosiken.jp" || url.host === request.hostname) {
-          if (url.searchParams.has("plugin") && url.searchParams.get("plugin") === "ref"){
+          if (url.searchParams.has("plugin") && url.searchParams.get("plugin") === "ref") {
             $(a).replaceWith(`<img src="/ref${request.path}/${url.searchParams.get('page')}/${url.searchParams.get('src')}" alt="${alt}" title="${title}" width=${w} height=${h} />`);
-          } else if (url.searchParams.has("plugin") && url.searchParams.has("refer") && url.searchParams.get("plugin") === "attach"){
+          } else if (url.searchParams.has("plugin") && url.searchParams.has("refer") && url.searchParams.get("plugin") === "attach") {
             $(a).replaceWith(`<a href="/ref${request.path}/${url.searchParams.get('refer')}/${url.searchParams.get('openfile')}" alt="${alt}" title="${title}" width=${w} height=${h} />`);
           }
         }
@@ -92,8 +84,7 @@ app.get("/petc(3gou)?4?/", (request, response) => {
     const about = $("#pukiwiki-about").text();
     const lastUpdate = about.slice(about.indexOf("このページの最終更新 : "), about.indexOf("このページの最終更新 : ") + 36);
 
-    const template = fs.readFileSync(path + "/template.ejs", "utf-8");
-    const html = ejs.render(template, {
+    const html = ejs.render(pageTemplate(), {
       wikiTitle: "プチコンまとめArchive",
       pageTitle: pageTitle,
       base: request.path,
@@ -105,22 +96,30 @@ app.get("/petc(3gou)?4?/", (request, response) => {
   } else if (fs.existsSync(path + "/websites_utf8/wiki.hosiken.jp/" + page + "/" + page.slice(page.lastIndexOf("=") + 1))) {
     response.sendFile(path + "/websites_utf8/wiki.hosiken.jp/" + page + "/" + page.slice(page.lastIndexOf("=") + 1));
   } else {
-    const template = fs.readFileSync("template.ejs", "utf-8");
-    const html = ejs.render(template, {
+    const decodedAry = Encoding.urlDecode(request.originalUrl);
+    console.log(decodedAry);
+    const encoding = Encoding.detect(decodedAry);
+    const decodedStr = Encoding.codeToString(Encoding.convert(decodedAry, {from:encoding, to:"UNICODE"}));
+    console.log(decodedStr);
+    let eucEncode = Encoding.urlEncode(Encoding.convert(decodedAry, {from:encoding, to:"EUC-JP"})).replaceAll("%2F", "/").replace("%3F", "?");
+    if(!pageExists(eucEncode)){
+      eucEncode = "";
+    }
+    const html = ejs.render(pageTemplate(), {
       wikiTitle: "プチコンまとめArchive",
       pageTitle: "ページが見つかりません",
       base: request.path,
-      body: `<p>ページ ${iconv.decode(queryToBuffer(page), "eucjp")} は プチコンまとめArchive に存在しません。</p>`,
+      body: `<p>ページ ${eucToStr(Encoding.urlDecode(request.originalUrl))} は プチコンまとめArchive に存在しません。</p>` +
+        (eucEncode ? `<p>ページ名はEUC-JPで指定する必要があります。もしかして: <a href="${eucEncode}">${decodedStr}</a></p>` : ""),
       lastUpdate: "",
       notes: ""
     });
-    response.send(html);
+    response.status(404).send(html);
   }
 });
 
 app.get("/", (request, response) => {
-  const template = fs.readFileSync(path + "/template.ejs", "utf-8");
-  const html = ejs.render(template, {
+  const html = ejs.render(pageTemplate(), {
     wikiTitle: "プチコンまとめArchive",
     pageTitle: "プチコンまとめArchive トップページ",
     base: request.path,
@@ -129,6 +128,18 @@ app.get("/", (request, response) => {
     notes: ""
   });
   response.send(html);
+});
+
+app.use((request, response) => {
+  const html = ejs.render(pageTemplate(), {
+    wikiTitle: "プチコンまとめArchive",
+    pageTitle: "ページが見つかりません",
+    base: request.path,
+    body: request.method === "POST" ? `<p>編集やコメントの投稿は無効です。</p>` : `<p>URLが正しくありません。</p>`,
+    lastUpdate: "",
+    notes: ""
+  });
+  response.status(404).send(html);
 });
 
 module.exports = app;
